29782a11ec179e0e99fb7fd698d7ef9e267d98ae
Thibaud ROBERT <siouxthibaud@hotmail.fr>
2017-08-20 20:34:53 +0200

    add bootstap in css + we can select the size of the png dowloaded

--- Website/js/chessdiag.js
+++ Website/js/chessdiag.js
@@ -1,6 +1,6 @@
 ﻿var board;
 
-var main = function () {
+var init = function () {
     board = ChessBoard('board', {
         draggable: true,
         dropOffBoard: 'trash',
@@ -9,9 +9,68 @@ var main = function () {
     $('#startBtn').on('click', board.start);
     $('#clearBtn').on('click', board.clear);
     $('#download').on('click', download);
+    $("input[name=typefile]").click(function () {
+        var typefile = $('input[name=typefile]:checked', '#typefile').val();
+
+        if (typefile === "fen") {
+            $("#filesize").addClass("hidden");
+        }
+        else if (typefile === "png") {
+            $("#filesize").removeClass("hidden");
+        }
+    });
 };
 
-var saveBlob = function (blob, filename) {
+var download = function () {
+    var typefile = $('input[name=typefile]:checked', '#typefile').val();
+
+    if (typefile === "fen") {
+        downloadFen();
+    }
+    else if (typefile === "png") {
+        downloadImage();
+    }
+};
+
+var downloadImage = function () {
+    var chessboard = $("div#board div[class^='board-']");
+    var typefile = $('input[name=size]:checked', '#size').val();
+    var size = Number(typefile) * (96 / 2.54);
+
+    html2canvas(chessboard[0], {
+        onrendered: function (canvas) {
+            canvas = resizeCanvas(canvas, size, size);
+            canvas.toBlob(function (blob) {
+                downloadBlob(blob, "chessdiag.png");
+            })
+        },
+    });
+
+    //https://html2canvas.hertzen.com/examples.html
+};
+
+var downloadFen = function () {
+    var fen = board.fen();
+
+    downloadFile("chessdiag.fen", "text/fen", fen);
+};
+
+var downloadFile = function (filename, type, data) {
+    var blob = new Blob([data], { type: type });
+    downloadBlob(blob, filename);
+};
+
+var resizeCanvas = function (canvas, width, height) {
+    var tempCanvas = document.createElement('canvas');
+    tempCanvas.width = width;
+    tempCanvas.height = height;
+
+    tempCanvas.getContext('2d').drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, width, height);
+
+    return tempCanvas;
+};
+
+var downloadBlob = function (blob, filename) {
     if (window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveBlob(blob, filename);
     }
@@ -22,43 +81,5 @@ var saveBlob = function (blob, filename) {
         document.body.appendChild(elem);
         elem.click();
         document.body.removeChild(elem);
-    }
-};
-
-var save = function (filename, type, data) {
-    var blob = new Blob([data], { type: type });
-    saveBlob(blob, filename);
-};
-
-
-var downloadFen = function () {
-    var fen = board.fen();
-
-    save("chessdiag.fen", "text/fen", fen);
-};
-
-var downloadImage = function () {
-    var chessboard = $("div#board div[class^='board-']");
-
-    html2canvas(chessboard[0], {
-        onrendered: function (canvas) {
-            var data = canvas.toBlob(function (blob) {
-                saveBlob(blob, "chessdiag.png");
-            })
-        }
-    });
-
-    //https://html2canvas.hertzen.com/examples.html
-};
-
-
-var download = function () {
-    var typefile = $('input[name=typefile]:checked', '#typefile').val();
-
-    if (typefile === "fen") {
-        downloadFen();
-    }
-    else if (typefile === "png") {
-        downloadImage();
     }
 };
